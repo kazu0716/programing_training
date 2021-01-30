1414b88c527b7ecd1c9af877009310229a93fe25
kazu0716 <kazu2055.tennis@gmail.com>
2021-01-31 02:06:09 +0900

    add: how many divisors

---
+++ AOJ/ITP1/how_many_divisors.py
@@ -0,0 +1,19 @@
+def divisors(n): 
+    i = 1
+    table = []
+    while i * i <= n:
+        if n%i == 0:
+            table.append(i)
+            table.append(n//i)
+        i += 1
+    table = list(set(table))
+    return table
+
+l = list(map(int, input().split()))
+d = divisors(l[2])
+
+n = 0
+for i in d:
+    if i >= l[0] and i <= l[1]:
+        n += 1
+print(n)
