14fe42b3e848fabea7bdb0c311433b9a3dc412a2
kazu0716 <kazu2055.tennis@gmail.com>
2021-02-02 23:33:28 +0900

    add: print reactangle

---
+++ AOJ/ITP1/print_reactangle.py
@@ -0,0 +1,9 @@
+while True:
+    l = list(map(int, input().split()))
+    if l != [0, 0]:
+       for i in range(l[0]):
+            r = "".join(["#" for j in range(l[1])])
+            print(r)
+       print("")
+    else:
+        break
