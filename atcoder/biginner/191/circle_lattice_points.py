bc234c058d9438c4177d9eb2efc17286bbdea5e4
kazu0716 <kazu2055.tennis@gmail.com>
2021-02-09 00:07:54 +0900

    fix: circle lattice points

--- atcoder/biginner/191/circle_lattice_points.py
+++ atcoder/biginner/191/circle_lattice_points.py
@@ -1,10 +1,13 @@
 from decimal import Decimal, Context, ROUND_FLOOR, setcontext
+from sys import float_info
 
-# NOTE: sqrtする際の誤差をround_floorで切り捨てるので、微小値を足すことで精度を保つ(多分)
+# NOTE: sqrtする際の誤差をround_floorで切り捨てるので、eで補完する
 # ref: https://marco-note.net/abc-191-work-log/
 C = Context(rounding=ROUND_FLOOR)
 setcontext(C)
-d = Decimal('1E-9')
+# NOTE: pythonの計算機イプシロン
+# ref: https://qiita.com/ikuzak/items/1332625192daab208e22
+e = Decimal(str(float_info.epsilon))
 
 X, Y, R = map(Decimal, input().split())
 zero = Decimal("0")
@@ -13,7 +16,7 @@ ans = 0
 for x in range(int(X-R), int(X+R)+1):
     y2_ = R ** 2 - (X-x) ** 2
     if y2_ >= zero:
-        y_ = C.sqrt(y2_+d)
+        y_ = C.sqrt(y2_+e)
         y_min = Y - y_
         y_max = Y + y_
         ans += (int(y_max.quantize(zero)) - int(y_min.quantize(zero)))
