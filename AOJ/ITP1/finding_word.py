cb9e8456e630587219a3774746591485bf5703ea
kazu0716 <kazu2055.tennis@gmail.com>
2021-02-09 23:53:21 +0900

    add: finding a word

---
+++ AOJ/ITP1/finding_word.py
@@ -0,0 +1,11 @@
+w = input()
+ans = 0
+
+while True:
+    txt = input()
+    if txt == "END_OF_TEXT":
+        break
+    for s in txt.split():
+        if s.lower() == w.lower():
+            ans +=1
+print(ans)
