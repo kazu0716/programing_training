f52caef6e49eed3b8a466da9ca1213d6c703823e
kazu0716 <kazu2055.tennis@gmail.com>
2021-02-10 01:31:41 +0900

    add: shuffle

---
+++ AOJ/ITP1/shuffle.py
@@ -0,0 +1,17 @@
+ans = []
+cards = ""
+
+while True:
+    s = input()
+    if s.isalpha() or s == "-":
+        if cards != "":
+            ans.append(cards)
+        if s == "-": 
+            break
+        cards = s
+    else:
+        m = int(s)
+        for _ in range(m):
+            n = int(input())
+            cards = cards[n:] + cards[0:n]
+print(*ans,sep="\n")
