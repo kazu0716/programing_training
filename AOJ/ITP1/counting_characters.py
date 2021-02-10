931037022923f2166443cbedae3ac960888a9a81
kazu0716 <kazu2055.tennis@gmail.com>
2021-02-10 23:32:11 +0900

    fix: counting charactors of input

--- AOJ/ITP1/counting_characters.py
+++ AOJ/ITP1/counting_characters.py
@@ -1,19 +1,13 @@
-from time import time
-
-t1 = time()
-lines = []
-
-# TODO: 入力が上手くいかないが、countできてるので飛ばす
-while True:
-    line = input().lower()
-    lines.append(line)
-    t2 = time()
-    if t2 - t1 >= 0.5:
-        break
-
+txt = ""
 alphabets = "abcdefghijklmnopqrstuvwxyz"
 
-ans = [" ".join(lines).count(c) for c in alphabets]
+while True:
+    try:
+        txt += input().lower()
+    except EOFError:
+        break
+
+ans = [txt.count(c) for c in alphabets]
 
 for i in range(len(alphabets)):
     print("{} : {}".format(alphabets[i], ans[i]))
