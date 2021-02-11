b2a626dc88c9aac0682e64a3f98fb0a4afa0075d
kazu0716 <kazu2055.tennis@gmail.com>
2021-02-12 00:04:13 +0900

    add: comment at day dream

--- atcoder/biginners_selection/day_dream.py
+++ atcoder/biginners_selection/day_dream.py
@@ -1,4 +1,4 @@
-if input().replace("eraser", "").replace("erase", "").replace("dreamer", "").replace("dream", ""):
-    print("NO")
-else:
-    print("YES")
+# NOTE: 困るのは'dreameraser','dreamerase'なので、それぞれ先に消せば良い、反転して愚直に消しても良い
+# ref: https://iryond.hatenablog.com/entry/2019/04/13/103733
+print("NO" if input().replace("eraser", "").replace(
+    "erase", "").replace("dreamer", "").replace("dream", "") else "YES")
