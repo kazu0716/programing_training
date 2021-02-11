20f5d80d241af9a6b1d0fcb0f4eeef70916d61bb
kazu0716 <kazu2055.tennis@gmail.com>
2021-02-11 23:12:43 +0900

    fix: card game for two

--- atcoder/biginners_selection/card_game_for_two.py
+++ atcoder/biginners_selection/card_game_for_two.py
@@ -1,14 +1,14 @@
 N = int(input())
 A = list(map(int, input().split()))
 
+A.sort(reverse=True)
 ans = []
 
 for i in range(len(A)):
     if i % 2 == 0:
-        ans.append(max(A))
+        ans.append(A[0])
     else:
-        ans.append(-max(A))
-
-    A.remove(max(A))
+        ans.append(-A[0])
+    A.pop(0)
 
 print(sum(ans))
