3edd6b22265db66fa3cf99ef0e3c1049e0f95020
kazu0716 <kazu2055.tennis@gmail.com>
2021-02-23 00:23:56 +0900

    fix: osenbei

--- atcoder/trainings/full_searches/osenbei.py
+++ atcoder/trainings/full_searches/osenbei.py
@@ -3,12 +3,13 @@ S = [list(map(int, input().split())) for _ in range(R)]
 
 ans = 0
 range_r, range_c = range(R), range(C)
+S_ini = [[0] * C for _ in range_r]
+
 
 for i in range(2 ** R):
     bit = bin(i)[2:].rjust(R, "0")
     s = 0
-    S_copy = [[0] * C for _ in range_r]
-
+    S_copy = S_ini
     for j, b in enumerate(list(bit)):
         if b == "1":
             sj = S[j]
