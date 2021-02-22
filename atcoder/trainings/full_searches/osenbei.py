e3cb217553f450a372c0fe841246dc6fd176ca13
kazu0716 <kazu2055.tennis@gmail.com>
2021-02-22 23:34:39 +0900

    add: osenbei

---
+++ atcoder/trainings/full_searches/osenbei.py
@@ -0,0 +1,27 @@
+R, C = map(int, input().split())
+S = [list(map(int, input().split())) for _ in range(R)]
+
+ans = 0
+range_r, range_c = range(R), range(C)
+
+for i in range(2 ** R):
+    bit = bin(i)[2:].rjust(R, "0")
+    s = 0
+    S_copy = [[0] * C for _ in range_r]
+
+    for j, b in enumerate(list(bit)):
+        if b == "1":
+            sj = S[j]
+            S_copy[j] = list(map(lambda x: (x+1) % 2, sj))
+        else:
+            S_copy[j] = S[j]
+
+    for c in range_c:
+        cnt = 0
+        for r in range_r:
+            if S_copy[r][c] == 1:
+                cnt += 1
+        s += max(cnt, R-cnt)
+    ans = max(ans, s)
+
+print(ans)
