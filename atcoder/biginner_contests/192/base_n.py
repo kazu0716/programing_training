71e7562f6183efa9e4a320b27c9c3a9bff72565e
kazu0716 <kazu2055.tennis@gmail.com>
2021-02-24 16:17:28 +0900

    fix: base n

--- atcoder/biginner_contests/192/base_n.py
+++ atcoder/biginner_contests/192/base_n.py
@@ -1,36 +1,39 @@
-X, M = input(), int(input())
-
-d = max(list(map(int, list(X))))
+from collections import deque
 
 
-def base10from(num, b):
-    n = 0
-    numlist = list(num)
-    while (numlist):
-        n *= b
-        n += int(numlist.pop(0))
-    return n
+def base10_from(num, base):
+    i, s, que = 0, 0, deque(list(str(num)))
+    while que:
+        s += int(que.pop()) * base ** i
+        i += 1
+    return s
 
 
-def binary_search(x, m, _d):
+def binary_search(x, m, d):
     if len(x) == 1:
-        if int(x) <= m:
-            return 1
-        else:
+        if int(x) > m:
             return 0
+        else:
+            return 1
 
-    low = _d
+    low = d
     high = m + 1
 
     while high - low > 1:
         mid = (low + high) // 2
-        guess = base10from(x, mid)
-
+        guess = base10_from(x, mid)
         if guess > m:
             high = mid
         else:
             low = mid
-    return low - _d
+    return low - d
 
 
-print(binary_search(X, M, d))
+def main():
+    X, M = input(), int(input())
+    d = max(list(map(int, list(X))))
+    print(binary_search(X, M, d))
+
+
+if __name__ == '__main__':
+    main()
