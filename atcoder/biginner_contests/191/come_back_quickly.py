4a2f4d35994dc1fb33554f9428ca04b307344c1f
kazu0716 <kazu2055.tennis@gmail.com>
2021-02-27 20:55:21 +0900

    add: come back quickly
    ref: https://atcoder.jp/contests/abc191/submissions/20517051

--- atcoder/biginner_contests/191/come_back_quickly.py
+++ atcoder/biginner_contests/191/come_back_quickly.py
@@ -1,32 +1,50 @@
-from heapq import heappush, heappop
+from heapq import heappop, heappush
+
+INF = float("inf")
 
 
-N, M = map(int, input().split())
-# TODO: 2倍する意味を考える
-edge = [[] for _ in range(2 * N)]
+def dijkstra(s, n, adj):
+    dist = [INF] * n
+    hq = [(0, s)]
+    dist[s] = 0
+    seen = [False] * n
+    while hq:
+        v = heappop(hq)[1]
+        seen[v] = True
+        for to, cost in adj[v]:
+            if not seen[to] and dist[v] + cost < dist[to]:
+                dist[to] = dist[v] + cost
+                heappush(hq, (dist[to], to))
+    return dist
 
-for _ in range(M):
-    a, b, c = map(int, input().split())
-    a -= 1; b -= 1
-    edge[a].append((b, c))
 
-ans = [-1] * N
-path = [-1] * (2 * N)
-for i in range(N):
-    q = []
-    # 自己ループを考慮して、最初にまず1手目の町をすべてキューにいれる
-    for nv, c in edge[i]:
-        heappush(q, (c, nv))
-    # TODO: ダイクストラ法を理解する
-    while q:
-        s, v = heappop(q)
-        # NOTE: ゴールはスタート地点のため、breakする
-        if v == i:
-            ans[i] = s
-            break
-        elif path[v] < i:
-            path[v] = i
-            for nv, c in edge[v]:
-                ns = s + c
-                heappush(q, (ns, nv))
-print(*ans, sep='\n')
+def main():
+    N, M = map(int, input().split())
+    adj, ans, out = [[]for _ in range(
+        N)], [INF] * N, [[INF] * N for _ in range(N)]
+
+    for _ in range(M):
+        a, b, c = map(int, input().split())
+        adj[a-1].append((b-1, c))
+        if a-1 == b-1:
+            ans[a-1] = c
+
+    for s in range(N):
+        for dst, cost in enumerate(dijkstra(s, N, adj)):
+            if s != dst:
+                out[s][dst] = min(out[s][dst], cost)
+
+    for i, o in enumerate(out):
+        for j, cost in enumerate(o):
+            if cost != INF:
+                ans[i] = min(ans[i], cost + out[j][i])
+
+    for a in ans:
+        if a == INF:
+            print(-1)
+        else:
+            print(a)
+
+
+if __name__ == '__main__':
+    main()
