9e164706bc0d55be322d4d136df8af02fbeb073c
kazu0716 <kazu2055.tennis@gmail.com>
2021-03-03 02:22:09 +0900

    fix: snuke festival

--- atcoder/trainings/binary_searches/sunuke_festival.py
+++ atcoder/trainings/binary_searches/sunuke_festival.py
@@ -1,59 +1,17 @@
-def search_a(_list, item):
-    low, high = 0, len(_list) - 1
+from bisect import bisect_left, bisect_right
 
-    while high >= low:
-        mid = (low + high) // 2
-        guess_1 = _list[mid]
-        guess_2 = _list[mid+1]
+N = int(input())
+A, B, C = list(map(int, input().split())), list(
+    map(int, input().split())), list(map(int, input().split()))
 
-        if guess_1 < item and item <= guess_2:
-            return mid
-        if guess_1 >= item:
-            high = mid - 1
-        else:
-            low = mid + 1
-    return -1
+ans = 0
 
+A.sort()
+C.sort()
 
-def search_c(_list, item):
-    low, high = 0, len(_list) - 1
+for b in B:
+    idx_a = bisect_left(A, b)
+    idx_c = bisect_right(C, b)
+    ans += idx_a * (N - idx_c)
 
-    while high >= low:
-        mid = (low + high) // 2
-        guess_1 = _list[mid]
-        guess_2 = _list[mid + 1]
-
-        if guess_1 <= item and item < guess_2:
-            return mid+1
-        if guess_1 > item:
-            high = mid - 1
-        else:
-            low = mid + 1
-    return -1
-
-
-def main():
-    N = int(input())
-    A, B, C = list(map(int, input().split())), list(
-        map(int, input().split())), list(map(int, input().split()))
-
-    A.append(0)
-    A.append(pow(10, 10))
-    A.sort()
-
-    C.append(0)
-    C.append(pow(10, 10))
-    C.sort()
-
-    ans = 0
-
-    for b in B:
-        idx_a = search_a(A, b)
-        idx_c = search_c(C, b)
-        ans += idx_a * (N + 1 - idx_c)
-
-    print(ans)
-
-
-if __name__ == '__main__':
-    main()
+print(ans)
