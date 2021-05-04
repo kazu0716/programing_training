6a9365ad589319eb7b7b55e6e8b9e24147e34262
kazu0716 <kazu2055.tennis@gmail.com>
2021-05-05 01:29:00 +0900

    add: ABC199 a,b,c

---
+++ atcoder/biginner_contests/199/b.py
@@ -0,0 +1,16 @@
+N = int(input())
+A = tuple(map(int,input().split()))
+B = tuple(map(int,input().split()))
+
+mi,ma = 0, 10000
+
+for i in range(N):
+    a, b = A[i],B[i]
+    mi , ma = max(min(a,b),mi),min(max(a,b),ma)
+
+ans = ma-mi+1
+
+if ans > 0:
+    print(ans)
+else:
+    print(0)
