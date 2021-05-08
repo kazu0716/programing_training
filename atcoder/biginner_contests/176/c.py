93ea27c958edeb3519d2dadeb35a1f8b12f0bea2
kazu0716 <kazu2055.tennis@gmail.com>
2021-05-09 01:17:18 +0900

    add: ABC176 a,b,c

---
+++ atcoder/biginner_contests/176/c.py
@@ -0,0 +1,12 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+ans = 0
+
+for i in range(N-1):
+    if A[i] > A[i+1]:
+        dif = A[i] - A[i+1]
+        A[i+1] += dif
+        ans += dif
+
+print(ans)
