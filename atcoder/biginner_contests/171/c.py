2175dcf500aa3849d9e6a5a8e5665757843e1bea
kazu0716 <kazu2055.tennis@gmail.com>
2021-05-13 16:37:17 +0900

    fix: ABC171 c
    ref: https://tanuhack.com/num2alpha-alpha2num/

--- atcoder/biginner_contests/171/c.py
+++ atcoder/biginner_contests/171/c.py
@@ -5,9 +5,9 @@ def solve(n):
     if n <= 26:
         return chr(96 + n)
     elif n % 26 == 0:
-        return solve(n // 26 - 1)+chr(122)
+        return solve(n // 26 - 1) + "z"
     else:
-        return solve(n // 26)+chr(96 + n % 26)
+        return solve(n // 26) + chr(96 + n % 26)
 
 
 print(solve(N))
