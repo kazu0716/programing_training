ecc54aa4bc8cec1271d506bcd63022062475e034
kazu0716 <kazu2055.tennis@gmail.com>
2021-07-06 01:37:28 +0900

    fix: dir name

---
+++ AOJ/GRL_1/single_source_shortest_path.py
@@ -0,0 +1,36 @@
+from heapq import heappop, heappush
+
+INF = float("inf")
+
+
+def dijkstra(s, n, adj):
+    dist = [INF] * n
+    hq = [(0, s)]
+    dist[s] = 0
+    seen = [False] * n
+    while hq:
+        v = heappop(hq)[1]
+        seen[v] = True
+        for to, cost in adj[v]:
+            if not seen[to] and dist[v] + cost < dist[to]:
+                dist[to] = dist[v] + cost
+                heappush(hq, (dist[to], to))
+    return dist
+
+
+def main():
+    V, E, r = map(int, input().split())
+    adj = [[] for _ in range(V)]
+    for _ in range(E):
+        s, t, d = map(int, input().split())
+        adj[s].append((t, d))
+
+    for d in dijkstra(r, V, adj):
+        if d == float("inf"):
+            print("INF")
+        else:
+            print(d)
+
+
+if __name__ == '__main__':
+    main()
