d4083e6074fbeb2213b8b518b3803204f0dd0ebb
kazu0716 <kazu2055.tennis@gmail.com>
2021-08-28 13:55:06 +0900

    fix: ABC173 d

--- atcoder/ABC/173/d.py
+++ atcoder/ABC/173/d.py
@@ -2,11 +2,11 @@ from math import ceil
 
 N = int(input())
 A = list(map(int, input().split()))
-A.sort()
+A.sort(reverse=True)
 
 ans = 0
 
 for i in range(N-1):
-    ans += A[N - ceil(i/2) - 1]
+    ans += A[ceil(i/2)]
 
 print(ans)
