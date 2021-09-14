aa969783a578041caed9801a5defee2eea0a8347
kazu0716 <kazu2055.tennis@gmail.com>
2021-09-14 23:14:16 +0900

    fix: ABC170 d

--- atcoder/ABC/170/d.py
+++ atcoder/ABC/170/d.py
@@ -4,19 +4,18 @@ N = int(input())
 A = list(map(int, input().split()))
 A.sort()
 
-ans, ma = 0, max(A)
+ans, ma = 0, A[-1]
 div = set([])
 
 for i in range(0, N):
     a = A[i]
-    if i + 1 != bisect_right(A, a):
+    if i+1 != bisect_right(A, a):
         div.add(a)
         continue
-    for j in range(2*a, ma+1, a):
-        idx = bisect_left(A, j)
-        if A[idx] == j:
-            div.add(j)
     if a not in div:
         ans += 1
+    for j in range(2*a, ma+1, a):
+        if A[bisect_left(A, j)] == j:
+            div.add(j)
 
 print(ans)
