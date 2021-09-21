638356cb0db8e73e3db1a8077c57b7f16417dd53
kazu0716 <kazu2055.tennis@gmail.com>
2021-09-22 01:41:15 +0900

    fix: educationalDP e

--- atcoder/educational_dp/e.py
+++ atcoder/educational_dp/e.py
@@ -1,24 +1,16 @@
 from collections import defaultdict
 
 N, W = map(int, input().split())
-dp = [defaultdict(int) for _ in range(N+1)]
+INF = pow(10, 12)
+dp = [defaultdict(lambda: INF) for _ in range(N+1)]
 dp[0][0] = 0
 
 for i in range(1, N+1):
     w, v = map(int, input().split())
     for j in tuple(dp[i-1].keys()):
-        if dp[i][j] > 0:
-            dp[i][j] = min(dp[i-1][j], dp[i][j])
-        else:
-            dp[i][j] = dp[i-1][j]
-
+        dp[i][j] = min(dp[i-1][j], dp[i][j])
         if dp[i-1][j] + w > W:
             continue
-
-        if dp[i-1][j+v] > 0:
-            dp[i][j+v] = min(dp[i-1][j] + w, dp[i-1][j+v])
-        else:
-            del dp[i-1][j+v]
-            dp[i][j+v] = dp[i-1][j] + w
+        dp[i][j+v] = min(dp[i-1][j] + w, dp[i-1][j+v])
 
 print(max(dp[-1].keys()))
