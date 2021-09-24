a3da49e7edb0a60619b2f574c8c5a9d077974836
kazu0716 <kazu2055.tennis@gmail.com>
2021-09-24 14:23:49 +0900

    fix: educationalDP i

--- atcoder/educational_dp/i.py
+++ atcoder/educational_dp/i.py
@@ -1,16 +1,11 @@
 N = int(input())
 P = [0.0] + list(map(float, input().split()))
 dp = [[0.0] * (N+1) for _ in range(N+1)]
+dp[0][0] = 1
 
 for i in range(1, N+1):
     for j in range(N+1):
-        p, q = P[i], 1.0 - P[i]
-        if i == 1 and j == 1:
-            dp[i][j] = p
-        elif i == 1 and j == 0:
-            dp[i][j] = q
-        else:
-            dp[i][j-1] += dp[i-1][j-1] * q
-            dp[i][j] += dp[i-1][j-1] * p
+        dp[i][j-1] += dp[i-1][j-1] * (1.0 - P[i])
+        dp[i][j] += dp[i-1][j-1] * P[i]
 
 print(sum([dp[-1][j] for j in range(N//2+1, N+1)]))
