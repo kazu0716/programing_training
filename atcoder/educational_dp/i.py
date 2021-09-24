7ee3cbd1e5ea1e9e7749d0c566ad61d4bb9af7ad
kazu0716 <kazu2055.tennis@gmail.com>
2021-09-24 09:52:36 +0900

    fix: educationalDP i

--- atcoder/educational_dp/i.py
+++ atcoder/educational_dp/i.py
@@ -1,13 +1,10 @@
-from math import ceil
-
 N = int(input())
-ZERO = 0.0
-P = [ZERO] + list(map(float, input().split()))
-dp = [[ZERO] * (N+1) for _ in range(N+1)]
+P = [0.0] + list(map(float, input().split()))
+dp = [[0.0] * (N+1) for _ in range(N+1)]
 
 for i in range(1, N+1):
     for j in range(N+1):
-        p, q = P[i], 1-P[i]
+        p, q = P[i], 1.0 - P[i]
         if i == 1 and j == 1:
             dp[i][j] = p
         elif i == 1 and j == 0:
@@ -16,8 +13,4 @@ for i in range(1, N+1):
             dp[i][j-1] += dp[i-1][j-1] * q
             dp[i][j] += dp[i-1][j-1] * p
 
-ans = ZERO
-for j in range(ceil(N/2), N+1):
-    ans += dp[-1][j]
-
-print(ans)
+print(sum([dp[-1][j] for j in range(N//2+1, N+1)]))
