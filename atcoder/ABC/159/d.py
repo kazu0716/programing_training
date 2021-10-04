576d577bbc5f249b8154e5f60cdffc305894edce
kazu0716 <kazu2055.tennis@gmail.com>
2021-10-05 01:40:01 +0900

    fix: ABC159 d

--- atcoder/ABC/159/d.py
+++ atcoder/ABC/159/d.py
@@ -1,27 +1,21 @@
 from collections import Counter
 
+
+def comb(n):
+    return n*(n-1)//2
+
+
 N = int(input())
 A = list(map(int, input().split()))
 counter = Counter(A)
-ans = []
-
-
-def comb(n):
-    if n > 2:
-        return n*(n-1)//2
-    elif n == 2:
-        return 1
-    else:
-        return 0
-
-
 s = sum(map(comb, counter.values()))
+ans = []
 
 for i in range(N):
     a = A[i]
     n1, n2 = counter[a], counter[a]-1
     c1, c2 = comb(n1), comb(n2)
-    dif = c1-c2
-    ans.append(s-dif)
+    diff = c1-c2
+    ans.append(s-diff)
 
 print(*ans, sep="\n")
