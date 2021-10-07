677bcdacebd62de30937a1df1864ca80bbac6923
kazu0716 <kazu2055.tennis@gmail.com>
2021-10-08 02:04:01 +0900

    fix: ABC152 d

--- atcoder/ABC/152/d.py
+++ atcoder/ABC/152/d.py
@@ -8,8 +8,8 @@ for i in range(1, N+1):
     top, btm = int(s[0]), int(s[-1])
     dp[top][btm] += 1
 
-for i in range(10):
-    for j in range(10):
-        ans += dp[i][j] * dp[j][i]
+for top in range(10):
+    for btm in range(10):
+        ans += dp[top][btm] * dp[btm][top]
 
 print(ans)
