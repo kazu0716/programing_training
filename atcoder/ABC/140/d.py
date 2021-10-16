0c66fc15ecfecd5dab4081837ee2c042af19bcfd
kazu0716 <kazu2055.tennis@gmail.com>
2021-10-16 16:39:29 +0900

    add: ABC140 d

--- atcoder/ABC/140/d.py
+++ atcoder/ABC/140/d.py
@@ -1,33 +1,21 @@
 N, K = map(int, input().split())
-S = input()
+S = list(input())
 
 line = []
-pre, cnt = S[0], 1
-for i in range(1, len(S)):
-    if pre == S[i]:
-        cnt += 1
-    else:
-        line.append([len(line), pre, cnt])
-        cnt = 1
-    pre = S[i]
-line.append([len(line), pre, cnt])
-
-sorted_line = sorted(line, key=lambda x: x[2])
-for _ in range(K):
-    if not sorted_line:
-        break
-    idx, direction, n = sorted_line.pop()
-    line[idx][1] = "R" if direction == "L" else "L"
-
-ans = 0
-for _ in range(len(line)):
-    idx, direction, n = line.pop()
+for _ in range(len(S)):
+    s = S.pop()
     if not line:
-        ans += n-1
-        break
-    if line[-1][1] == direction:
-        line[-1][2] += n
+        line.append(s)
     else:
-        ans += n-1
+        if line[-1] == s:
+            continue
+        line.append(s)
+line = line[::-1]
 
+for _ in range(K):
+    for _ in range(2):
+        if len(line) > 1:
+            line.pop()
+
+ans = (N-1) - (len(line)-1)
 print(ans)
