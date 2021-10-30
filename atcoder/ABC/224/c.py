aa5656f5832c9bc89197fabd6f71be9df919a480
kazu0716 <kazu2055.tennis@gmail.com>
2021-10-30 14:47:23 +0900

    fix: ABC224 c

--- atcoder/ABC/224/c.py
+++ atcoder/ABC/224/c.py
@@ -12,8 +12,7 @@ for p1, p2, p3 in combinations(P, 3):
     p1_x, p1_y = p1[0], p1[1]
     p2_x, p2_y = p2[0], p2[1]
     p3_x, p3_y = p3[0], p3[1]
-    s = abs((p2_x-p1_x)*(p3_y-p1_y) - (p3_x-p1_x)*(p2_y-p1_y))/2
-    if s > 0:
+    if (p2_x-p1_x)*(p3_y-p1_y) != (p3_x-p1_x)*(p2_y-p1_y):
         ans += 1
 
 print(ans)
