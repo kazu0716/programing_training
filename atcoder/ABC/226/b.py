ecb26c8ade5a5e68c8c8a42223bddb1137b8fe76
kazu0716 <kazu2055.tennis@gmail.com>
2021-11-13 18:17:28 +0900

    fix: ABC226 b

--- atcoder/ABC/226/b.py
+++ atcoder/ABC/226/b.py
@@ -1,14 +1,8 @@
-from collections import defaultdict
-
 N = int(input())
-cnt = defaultdict(set)
+ans = set([])
 
 for _ in range(N):
     length, *line = map(int, input().split())
-    cnt[length].add(tuple(line))
+    ans.add(tuple(line))
 
-ans = 0
-for key in cnt:
-    ans += len(cnt[key])
-
-print(ans)
+print(len(ans))
