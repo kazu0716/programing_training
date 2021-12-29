10d2a9f48ad0a31fa417191e4798c480a53c0a25
kazu0716 <kazu2055.tennis@gmail.com>
2021-12-30 02:45:37 +0900

    fix: ABC153 e

--- atcoder/ABC/153/e.py
+++ atcoder/ABC/153/e.py
@@ -7,6 +7,8 @@ dp = [[INF]*(H+1) for _ in range(N+1)]
 dp[0][H] = 0
 for i in range(N):
     for h in range(H, -1, -1):
+        if dp[i][h] == INF:
+            continue
         dp[i+1][h] = min(dp[i+1][h], dp[i][h])
         hh = max(0, h - magic[i][0])
         dp[i][hh] = min(dp[i][hh], dp[i][h]+magic[i][1])
