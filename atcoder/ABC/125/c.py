b70308e303ed38c91165a57fd815717e9efb6729
kazu0716 <kazu2055.tennis@gmail.com>
2022-01-14 01:33:50 +0900

    add: ABC125 c

---
+++ atcoder/ABC/125/c.py
@@ -0,0 +1,17 @@
+from math import gcd
+
+N = int(input())
+A = list(map(int, input().split()))
+
+gl, gr = A[0], A[-1]
+sl, sr = [gl], [gr]
+for i in range(1, N):
+    gl, gr = gcd(gl, A[i]), gcd(gr, A[-i-1])
+    sl.append(gl)
+    sr.append(gr)
+
+ans = 0
+for i in range(N-1):
+    ans = max(ans,  gcd(sl[i], sr[N-3-i]))
+
+print(max(ans, sl[-2], sr[-2]))
