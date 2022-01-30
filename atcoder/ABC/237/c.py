b65b4194f3726943c8c0c6e9b49e5c02c3c484ca
kazu0716 <kazu2055.tennis@gmail.com>
2022-01-30 23:39:20 +0900

    fix: ABC237 c

--- atcoder/ABC/237/c.py
+++ atcoder/ABC/237/c.py
@@ -1,23 +1,22 @@
 from collections import deque
-from sys import exit, stdin
+from sys import exit
 
-S = deque(list(stdin.readline()[:-1]))
+S = deque(list(input()))
 front, back = 0, 0
+while S and S[0] == "a":
+    S.popleft()
+    front += 1
 while S and S[-1] == "a":
     S.pop()
     back += 1
 if not S:
     print("Yes")
-    exit()
-while S and S[0] == "a":
-    S.popleft()
-    front += 1
-if front > back:
+elif front > back:
     print("No")
-    exit()
-S = list(S)
-for i in range(len(S)//2+1):
-    if S[i] != S[-(i+1)]:
-        print("No")
-        exit()
-print("Yes")
+else:
+    S = list(S)
+    for i in range(len(S)//2+1):
+        if S[i] != S[-(i+1)]:
+            print("No")
+            exit()
+    print("Yes")
