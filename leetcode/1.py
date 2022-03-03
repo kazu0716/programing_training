0179e250752a2482ebe9fd4bc086f61c7c9e0b93
kazu0716 <kazu2055.tennis@gmail.com>
2022-03-04 01:18:10 +0900

    add: basic 1

--- leetcode/1.py
+++ leetcode/1.py
@@ -1,9 +1,20 @@
-from itertools import combinations
+from typing import Dict, List
 
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for idx1, idx2 in combinations(range(len(nums)), 2):
-            if nums[idx1] + nums[idx2] == target:
-                return [idx1, idx2]
-        return []
+        dic: Dict[int, int] = {}
+        for i, n in enumerate(nums):
+            key = target-n
+            if key in dic:
+                return [i, dic[key]]
+            else:
+                dic[n] = i
+        return [-1, -1]
+
+
+if __name__ == "__main__":
+    nums = list(map(int, input().replace("[", "").replace("]", "").split(",")))
+    target = int(input())
+    sol = Solution()
+    print(sol.twoSum(nums, target))
