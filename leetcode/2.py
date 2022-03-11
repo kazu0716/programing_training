2569b30693e985596b1ff39d3fe5277ffc6f54f4
kazu0716 <kazu2055.tennis@gmail.com>
2022-03-12 01:23:37 +0900

    fix: leetcode 2

--- leetcode/2.py
+++ leetcode/2.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
@@ -7,24 +10,53 @@ class ListNode:
 
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        str1, nxt1 = [], l1
-        while nxt1 is not None:
-            str1 += str(nxt1.val)
-            nxt1 = nxt1.next
-        str1 = int("".join(str1[::-1]))
+        carry, val = (l1.val+l2.val)//10, (l1.val+l2.val) % 10
+        head = cur = ListNode(val=val)
+        while l1.next or l2.next:
+            n = carry
+            if l1.next:
+                n += l1.next.val
+                l1 = l1.next
+            if l2.next:
+                n += l2.next.val
+                l2 = l2.next
+            carry, val = n//10, n % 10
+            cur.next = ListNode(val=val)
+            cur = cur.next
+        if carry != 0:
+            cur.next = ListNode(val=carry)
+        return head
 
-        str2, nxt2 = [], l2
-        while nxt2 is not None:
-            str2 += str(nxt2.val)
-            nxt2 = nxt2.next
-        str2 = int("".join(str2[::-1]))
 
-        ans = None
-        for s in str(str1 + str2):
-            if ans is None:
-                nxt = None
-            else:
-                nxt = ans
-            ans = ListNode(val=s, next=nxt)
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
-        return ans
+    def add(self, val: int) -> None:
+        if self.head is None:
+            self.head = ListNode(val)
+            return
+        cur = self.head
+        while cur.next is not None:
+            cur = cur.next
+        cur.next = ListNode(val)
+
+    def get_head(self) -> Optional[ListNode]:
+        return self.head
+
+    def show(self) -> None:
+        cur = self.head
+        while cur:
+            print(cur.val)
+            cur = cur.next
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    linked_list1, linked_list2 = LinkedList(), LinkedList()
+    for linked_list in (linked_list1, linked_list2):
+        input_list = list(map(int, list(input().replace("[", "").replace("]", "").split(","))))
+        for n in input_list:
+            linked_list.add(n)
+    solution.addTwoNumbers(linked_list1.get_head(), linked_list2.get_head())
+    linked_list.show()
