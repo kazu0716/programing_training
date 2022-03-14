0519b481d834d46ad6b6a5836da704098ed3419d
kazu0716 <kazu2055.tennis@gmail.com>
2022-03-15 02:04:35 +0900

    add: leetcode 373

--- leetcode/373.py
+++ leetcode/373.py
@@ -1,18 +1,32 @@
-from typing import List, Tuple
+from heapq import heappop, heappush
+from typing import List
 
 
-class Solution:
+class Solution(object):
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
-        ans: List[Tuple[int, int]] = []
-        for i in range(len(nums1)):
-            for j in range(len(nums2)):
-                s = nums1[i] + nums2[j]
-                if i < len(nums1)-1 and nums1[i+1] + nums2[j] < s:
-                    continue
-                if len(ans) == k:
-                    return sorted(ans, key=lambda x: x[0]+x[1])
-                ans.append((nums1[i], nums2[j]))
+        pairs: List[List[int]] = []
+        if len(nums1) > len(nums2):
+            for pair in self.kSmallestPairs(nums2, nums1, k):
+                pairs.append([pair[1], pair[0]])
+            return pairs
+
+        def push(i, j):
+            if i < len(nums1) and j < len(nums2):
+                heappush(min_heap, [nums1[i] + nums2[j], i, j])
+
+        min_heap = [[nums1[0] + nums2[0], 0, 0]]
+        while min_heap and len(pairs) < k:
+            _, i, j = heappop(min_heap)
+            pairs.append([nums1[i], nums2[j]])
+            push(i, j + 1)
+            if j == 0:
+                push(i + 1, 0)
+        return pairs
 
 
 if __name__ == "__main__":
-    pass
+    input_list1 = list(map(int, list(input().replace("[", "").replace("]", "").split(","))))
+    input_list2 = list(map(int, list(input().replace("[", "").replace("]", "").split(","))))
+    k = int(input())
+    sol = Solution()
+    print(sol.kSmallestPairs(input_list1, input_list2, k))
