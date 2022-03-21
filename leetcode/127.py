109828294a72e37309650e2b9ac2f965f7ad30c3
kazu0716 <kazu2055.tennis@gmail.com>
2022-03-22 03:07:24 +0900

    fix: leetcode 127

--- leetcode/127.py
+++ leetcode/127.py
@@ -7,17 +7,16 @@ class Solution:
         word_set = set(wordList)
         if endWord not in word_set:
             return 0
-        word_len = len(beginWord)
         queue = deque([(beginWord, 1)])
         while queue:
             cur, dist = queue.popleft()
-            if cur == endWord:
-                return dist
-            for i in range(word_len):
+            for i in range(len(beginWord)):
                 for j in range(26):
                     nxt = cur[:i] + chr(97+j) + cur[i+1:]
                     if nxt not in word_set:
                         continue
+                    if nxt == endWord:
+                        return dist+1
                     word_set.remove(nxt)
                     queue.append((nxt, dist+1))
         return 0
