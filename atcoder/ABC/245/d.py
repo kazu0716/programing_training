828802e009442cd0c65aaddc8bfd0e32d8a6f6b1
kazu0716 <kazu2055.tennis@gmail.com>
2022-03-27 04:11:59 +0900

    fix: ABC245 d
    ref: https://atcoder.jp/contests/abc245/editorial/3651

--- atcoder/ABC/245/d.py
+++ atcoder/ABC/245/d.py
@@ -1,8 +1,12 @@
-# NOTE: solved by Python3
-import numpy as np
-
 N, M = map(int, input().split())
-A = np.array(list(map(int, input().split()))[::-1])
-C = np.array(list(map(int, input().split()))[::-1])
-
-print(*map(int, list(np.polydiv(C, A)[0])[::-1]))
+A = list(map(int, input().split()))[::-1]
+C = list(map(int, input().split()))[::-1]
+B = []
+for i in range(M+1):
+    s = C[i]
+    for j in range(1, N+1):
+        if i-j < 0 or i-j >= len(B):
+            continue
+        s -= A[j]*B[i-j]
+    B.append(s//A[0])
+print(*B[::-1])
