4b2da2e12c5b3fd8effd7d959274e145f5cfa2a3
kazu0716 <kazu2055.tennis@gmail.com>
2022-04-08 02:11:27 +0900

    fix: leetcode 63

--- leetcode/63.py
+++ leetcode/63.py
@@ -5,15 +5,16 @@ from typing import List
 
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        if obstacleGrid[0][0] == 1:
+            return 0
         h, w = len(obstacleGrid), len(obstacleGrid[0])
         grid = [[0]*w for _ in range(h)]
-        grid[0][0] = 1 if obstacleGrid[0][0] == 0 else 0
+        grid[0][0] = 1
         for i in range(h):
             for j in range(w):
-                if i + 1 < h and obstacleGrid[i+1][j] == 0:
-                    grid[i+1][j] += grid[i][j]
-                if j + 1 < w and obstacleGrid[i][j+1] == 0:
-                    grid[i][j+1] += grid[i][j]
+                if (i == j == 0) or obstacleGrid[i][j] == 1:
+                    continue
+                grid[i][j] = (grid[i-1][j] if i > 0 else 0) + (grid[i][j-1] if j > 0 else 0)
         return grid[-1][-1]
 
 
