2c3b82ee3aea6c071fb8c8e29d9e055d7bf0b6bc
kazu0716 <kazu2055.tennis@gmail.com>
2022-04-08 22:33:02 +0900

    fix: leetcode 198

--- leetcode/198.py
+++ leetcode/198.py
@@ -3,16 +3,10 @@ from typing import List
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        dp = [0]*len(nums)
-        dp[0:2] = nums[0:2]
-        ret = 0
-        for i in range(len(nums)):
-            for d in (2, 3):
-                if i+d >= len(dp):
-                    ret = max(ret, dp[i])
-                else:
-                    dp[i+d] = max(dp[i+d], dp[i] + nums[i+d])
-        return ret
+        pre = cur = 0
+        for num in nums:
+            pre, cur = cur, max(pre + num, cur)
+        return cur
 
 
 if __name__ == "__main__":
