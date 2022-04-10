14465df59a26f713d8c10c2a0f5be3741c857bb6
kazu0716 <kazu2055.tennis@gmail.com>
2022-04-11 01:25:30 +0900

    fix: ABC247 b

--- atcoder/ABC/247/b.py
+++ atcoder/ABC/247/b.py
@@ -1,17 +1,19 @@
+from collections import defaultdict
+
 N = int(input())
-name = []
+names, name_hash = [], defaultdict(int)
 for _ in range(N):
     s, t = input().split()
-    name.append((s, t))
-for i in range(N):
-    s1, t1 = name[i]
-    is_s1, is_t1 = True, True
-    for s2, t2 in name[:i] + name[i+1:]:
-        if s1 == s2 or s1 == t2:
-            is_s1 = False
-        if t1 == s2 or t1 == t2:
-            is_t1 = False
-    if not is_s1 and not is_t1:
+    name_hash[s] += 1
+    name_hash[t] += 1
+    names.append((s, t))
+
+for s, t in names:
+    name_hash[s] -= 1
+    name_hash[t] -= 1
+    if name_hash[s] > 0 and name_hash[t] > 0:
         print("No")
         exit()
+    name_hash[s] += 1
+    name_hash[t] += 1
 print("Yes")
