13d51493a9cdd1f83ba835564c5ed56b2c927ee0
kazu0716 <kazu2055.tennis@gmail.com>
2022-04-26 01:14:17 +0900

    fix: leetcode 22

--- leetcode/22.py
+++ leetcode/22.py
@@ -1,23 +1,23 @@
+from sys import setrecursionlimit
 from typing import List
+
+setrecursionlimit(pow(10, 6))
 
 
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        paren = []
-        for i in range(pow(2, 2*n)):
-            cnt, p = 0, ""
-            for j in range(2*n):
-                if (i >> j) & 1:
-                    cnt += 1
-                    p += "("
-                else:
-                    cnt -= 1
-                    p += ")"
-                if cnt < 0:
-                    break
-            if cnt == 0:
-                paren.append(p)
-        return paren
+        def dfs(paren, cnt):
+            nonlocal parens
+            if len(paren) > 2*n or cnt < 0 or cnt - (2*n - len(paren)) > 0:
+                return
+            if len(paren) == 2*n and cnt == 0:
+                parens.append(paren)
+                return
+            for nxt in ("(", ")"):
+                dfs(paren + nxt, cnt + 1 if nxt == "(" else cnt - 1)
+        parens = []
+        dfs("", 0)
+        return parens
 
 
 if __name__ == "__main__":
