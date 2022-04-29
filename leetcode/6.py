e29be826a8a9c77817775cd032e560c963a27713
kazu0716 <kazu2055.tennis@gmail.com>
2022-04-29 23:20:27 +0900

    fix: leetcode 6

--- leetcode/6.py
+++ leetcode/6.py
@@ -1,17 +1,19 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        if numRows == 1:
+        if numRows == 1 or len(s) <= numRows:
             return s
-        zigzag = [[] for _ in range(numRows)]
-        cur, add = 0, 1
+        strings = [""] * numRows
+        row, step = 0, -1
         for ss in s:
-            zigzag[cur].append(ss)
-            if cur + add < 0:
-                add = 1
-            elif cur + add >= numRows:
-                add = -1
-            cur += add
-        ret = []
-        for i in range(numRows):
-            ret += zigzag[i]
-        return "".join(ret)
+            strings[row] += ss
+            if not(0 < row < numRows-1):
+                step *= -1
+            row += step
+        return "".join(strings)
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    s = "AB"
+    numRows = 1
+    print(sol.convert(s, numRows))
