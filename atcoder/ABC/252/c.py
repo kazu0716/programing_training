f61daca14a6618c42695265f5491f33f305e1b04
kazu0716 <kazu2055.tennis@gmail.com>
2022-05-21 23:28:01 +0900

    fix: ABC252 c

--- atcoder/ABC/252/c.py
+++ atcoder/ABC/252/c.py
@@ -1,20 +1,20 @@
-from collections import Counter, defaultdict
+from collections import defaultdict
 
 N = int(input())
-cnt = defaultdict(list)
+cnt = [defaultdict(int) for _ in range(10)]
 for _ in range(N):
     S = input()
     for i, s in enumerate(S):
-        cnt[int(s)].append(i)
-for k in cnt:
-    cnt[k] = Counter(cnt[k])
+        cnt[int(s)][i] += 1
 ans = pow(10, 18)
-for key in cnt:
+for i in range(10):
+    if not cnt[i]:
+        continue
     k_max, v_max = 0, 0
-    for k in cnt[key]:
-        if v_max < cnt[key][k]:
-            k_max, v_max = k, cnt[key][k]
-        elif v_max == cnt[key][k] and k_max < k:
+    for k in cnt[i]:
+        if v_max < cnt[i][k]:
+            k_max, v_max = k, cnt[i][k]
+        elif v_max == cnt[i][k] and k_max < k:
             k_max = k
     ans = min(ans, k_max + 10*(v_max-1))
 print(ans)
