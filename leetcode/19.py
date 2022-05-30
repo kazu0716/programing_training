ee42fe54cdc25ca8fabfc713b0ee3ee41cdb58c8
kazu0716 <kazu2055.tennis@gmail.com>
2022-05-31 01:27:48 +0900

    fix: leetcode 19

--- leetcode/19.py
+++ leetcode/19.py
@@ -11,14 +11,23 @@ class ListNode:
 
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        slow = fast = head
+        """
+        Time complexity : O(n)
+        Space complexity : O(1)
+
+        ref: https://leetcode.com/problems/remove-nth-node-from-end-of-list/discuss/8802/3-short-Python-solutions
+        """
+        fast = slow = head
+        # NOTE: proceed for step n times
         for _ in range(n):
             fast = fast.next
+        # NOTE: when n equals linkedlist length, delete head
         if fast is None:
             return head.next
+        # NOTE: proceed for slow until n+1 th node
         while fast.next:
-            slow = slow.next
-            fast = fast.next
+            slow, fast = slow.next, fast.next
+        # NOTE: delete nth node
         slow.next = slow.next.next
         return head
 
