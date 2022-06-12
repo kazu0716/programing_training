8a5e1192ebd797b9d5b23d113c320ee707611e8a
kazu0716 <kazu2055.tennis@gmail.com>
2022-06-13 00:55:16 +0900

    add: leetcode 131 complexity

--- leetcode/131.py
+++ leetcode/131.py
@@ -1,23 +1,29 @@
 from sys import setrecursionlimit
-from typing import List
+from typing import List, Optional
 
 setrecursionlimit(pow(10, 6))
 
 
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
+        """
+        Time complexity : O(n^2)
+        Space complexity : O(n^2)
+
+        ref: https://leetcode.com/problems/palindrome-partitioning/discuss/41973/Python-recursiveiterative-backtracking-solution
+        """
         ans = []
 
-        def traverse(string: str, palindromes: str) -> None:
+        def traverse(string: str, palindromes: List[str]) -> None:
             nonlocal ans
             if not string:
-                ans.append(palindromes[1:].split(","))
+                ans.append(palindromes)
                 return
             for i in range(len(string)):
                 if string[:i+1] == string[:i+1][::-1]:
-                    traverse(string[i+1:], f"{palindromes},{string[:i+1]}")
+                    traverse(string[i+1:], palindromes + [string[:i+1]])
 
-        traverse(s, "")
+        traverse(s, [])
         return ans
 
 
