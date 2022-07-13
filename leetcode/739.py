6e8813cf9a6ed677131c80b9f22549068d912ff3
kazu0716 <kazu2055.tennis@gmail.com>
2022-07-14 00:41:01 +0900

    add: leetcode 739

--- leetcode/739.py
+++ leetcode/739.py
@@ -15,8 +15,25 @@ class Solution:
             stack.append((t, i))
         return ans
 
+    def dailyTemperatures2(self, temperatures: List[int]) -> List[int]:
+        """
+        Time complexity : O(N)
+        Space complexity : O(1)
+        """
+        hottest, answer = 0, [0] * len(temperatures)
+        # NOTE: search tmp from back
+        for i in range(len(temperatures) - 1, -1, -1):
+            if temperatures[i] >= hottest:
+                hottest = temperatures[i]
+                continue
+            days = 1
+            while temperatures[i + days] <= temperatures[i]:
+                days += answer[i + days]
+            answer[i] = days
+        return answer
+
 
 if __name__ == "__main__":
     sol = Solution()
     temperatures = [73, 74, 75, 71, 69, 72, 76, 73]
-    print(sol.dailyTemperatures(temperatures))
+    print(sol.dailyTemperatures2(temperatures))
