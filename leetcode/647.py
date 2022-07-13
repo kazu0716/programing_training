cbaeee89c068d402a53fe328d30b0bfeec310c4f
kazu0716 <kazu2055.tennis@gmail.com>
2022-07-14 03:01:46 +0900

    add: another solution in leetcode 647

--- leetcode/647.py
+++ leetcode/647.py
@@ -12,8 +12,22 @@ class Solution:
                     cnt += 1
         return cnt
 
+    def countSubstrings2(self, s: str) -> int:
+        """
+        Time complexity : O(N)
+        Space complexity : O(1)
+        """
+        cnt, s = len(s), "".join([s if i == 0 else "#"+s for i, s in enumerate(s)])
+        for i in range(len(s)):
+            d = 1
+            while 0 <= i-d and i+d <= len(s)-1 and s[i-d] == s[i+d]:
+                if (s[i] == "#" or d > 1) and s[i-d] == s[i+d] != "#":
+                    cnt += 1
+                d += 1
+        return cnt
+
 
 if __name__ == "__main__":
     sol = Solution()
-    s = "aaa"
-    print(sol.countSubstrings(s))
+    s = "aaaaa"
+    print(sol.countSubstrings2(s))
