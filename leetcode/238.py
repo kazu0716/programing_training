a48c602c9d28afa082a9d703ab2734317b240280
kazu0716 <kazu2055.tennis@gmail.com>
2022-07-16 02:53:15 +0900

    fix: leetcode 238

--- leetcode/238.py
+++ leetcode/238.py
@@ -3,6 +3,10 @@ from typing import List
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        """
+        Time complexity : O(N)
+        Space complexity : O(1)
+        """
         s, cnt, idx = 1, 0, -1
         for i, num in enumerate(nums):
             if num == 0:
@@ -12,17 +16,9 @@ class Solution:
                 s *= num
             if cnt > 1:
                 return [0]*len(nums)
-        if cnt == 0:
-            for i in range(len(nums)):
-                nums[i] = s//nums[i]
-            return nums
-        else:
-            for i in range(len(nums)):
-                if i == idx:
-                    nums[i] = s
-                else:
-                    nums[i] = 0
-            return nums
+        for i in range(len(nums)):
+            nums[i] = s//nums[i] if cnt == 0 else s if i == idx else 0
+        return nums
 
 
 if __name__ == "__main__":
