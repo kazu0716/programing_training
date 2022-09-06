1d4113793992e5616dc9bed45e4551802941e9dd
kazu0716 <kazu2055.tennis@gmail.com>
2022-09-07 02:40:35 +0900

    fix: ABC267 E

--- atcoder/ABC/267/e.py
+++ atcoder/ABC/267/e.py
@@ -1,32 +1,26 @@
-def can_traverse(c: int, costs: list) -> bool:
-    stack, exsiting = set([i for i in range(N) if cost[i] <= c]), set([i for i in range(N)])
-    while stack:
-        cur = stack.pop()
-        exsiting.remove(cur)
-        for aj in edge[cur]:
-            if aj not in exsiting:
-                continue
-            costs[aj] -= A[cur]
-            if costs[aj] <= c and aj not in stack:
-                stack.add(aj)
-    return not exsiting
-
+from heapq import heapify, heappop, heappush
 
 N, M = map(int, input().split())
 A = list(map(int, input().split()))
 
-edge, cost = [[] for _ in range(N)], [0] * N
+edge, costs = [[] for _ in range(N)], [0] * N
 for _ in range(M):
     U, V = map(int, input().split())
     edge[U-1].append(V-1)
     edge[V-1].append(U-1)
-    cost[U-1] += A[V-1]
-    cost[V-1] += A[U-1]
-low, high = -1, max(cost)
-while high - low > 1:
-    mid = (low + high) // 2
-    if can_traverse(mid, cost.copy()):
-        high = mid
-    else:
-        low = mid
-print(high)
+    costs[U-1] += A[V-1]
+    costs[V-1] += A[U-1]
+
+hq, deleted, ans = [(c, i) for i, c in enumerate(costs)], [False] * N, 0
+heapify(hq)
+while hq:
+    cost, cur = heappop(hq)
+    if deleted[cur]:
+        continue
+    deleted[cur], ans = True, max(ans, cost)
+    for nxt in edge[cur]:
+        if deleted[nxt]:
+            continue
+        costs[nxt] -= A[cur]
+        heappush(hq, (costs[nxt], nxt))
+print(ans)
