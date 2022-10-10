7009e6acf17afbaf030a499526377cacd7c09763
kazu0716 <kazu2055.tennis@gmail.com>
2022-10-11 01:23:07 +0900

    fix: ABC272 c

--- atcoder/ABC/272/c.py
+++ atcoder/ABC/272/c.py
@@ -1,18 +1,16 @@
 N = int(input())
 A = sorted(list(map(int, input().split())), reverse=True)
-max_odd1, max_odd2, max_even1, max_even2 = -1, -1, -1, -1
+evens, odds, = [], []
 for a in A:
-    if a % 2 == 0:
-        if max_even1 == -1:
-            max_even1 = a
-        elif max_even2 == -1:
-            max_even2 = a
-    else:
-        if max_odd1 == -1:
-            max_odd1 = a
-        elif max_odd2 == -1:
-            max_odd2 = a
-if (max_even1 == -1 or max_even2 == -1) and (max_odd1 == -1 or max_odd2 == -1):
-    print(-1)
-else:
-    print(max(max_even1+max_even2, max_odd1+max_odd2))
+    if a % 2 == 0 and len(evens) < 2:
+        evens.append(a)
+    elif a % 2 == 1 and len(odds) < 2:
+        odds.append(a)
+    if len(evens) >= 2 and len(odds) >= 2:
+        break
+ans = -1
+if len(evens) >= 2:
+    ans = max(ans, sum(evens))
+if len(odds) >= 2:
+    ans = max(ans, sum(odds))
+print(ans)
