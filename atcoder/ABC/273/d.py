f2c2f5e2feff38420afd0cdd4b640e3fc70474b5
kazu0716 <kazu2055.tennis@gmail.com>
2022-10-16 04:09:25 +0900

    fix: ABC273 d

--- atcoder/ABC/273/d.py
+++ atcoder/ABC/273/d.py
@@ -1,5 +1,8 @@
 from bisect import bisect_left
 from collections import defaultdict
+from sys import stdin
+
+input = stdin.readline
 
 H, W, rs, cs = map(int, input().split())
 h_blocks, w_blocks = defaultdict(list), defaultdict(list)
@@ -9,29 +12,28 @@ for _ in range(N):
     h_blocks[r].append(c)
     w_blocks[c].append(r)
 for h in h_blocks:
+    h_blocks[h].append(0)
+    h_blocks[h].append(W+1)
     h_blocks[h].sort()
 for w in w_blocks:
+    w_blocks[w].append(0)
+    w_blocks[w].append(H+1)
     w_blocks[w].sort()
 Q = int(input())
+ans = []
 for _ in range(Q):
     d, l = input().split()
-    l = int(l)
-    if d == "U" or d == "D":
-        idx = bisect_left(w_blocks[cs], rs)
-        if d == "U":
-            block = 0 if idx == 0 else w_blocks[cs][idx-1]
-            nxt = max(1, rs - l, block + 1)
-        else:
-            block = H + 1 if idx >= len(w_blocks[cs]) else w_blocks[cs][idx]
-            nxt = min(H, rs + l, block - 1)
-        rs = nxt
+    if d == "U":
+        nxt = w_blocks[cs][bisect_left(w_blocks[cs], rs) - 1] + 1 if cs in w_blocks else 1
+        rs = max(rs - int(l), nxt)
+    elif d == "D":
+        nxt = w_blocks[cs][bisect_left(w_blocks[cs], rs)] - 1 if cs in w_blocks else H
+        rs = min(rs + int(l), nxt)
+    elif d == "L":
+        nxt = h_blocks[rs][bisect_left(h_blocks[rs], cs) - 1] + 1 if rs in h_blocks else 1
+        cs = max(cs - int(l), nxt)
     else:
-        idx = bisect_left(h_blocks[rs], cs)
-        if d == "L":
-            block = 0 if idx == 0 else h_blocks[rs][idx-1]
-            nxt = max(1, cs - l, block + 1)
-        else:
-            block = W + 1 if idx >= len(h_blocks[rs]) else h_blocks[rs][idx]
-            nxt = min(W, cs + l, block - 1)
-        cs = nxt
-    print(rs, cs)
+        nxt = h_blocks[rs][bisect_left(h_blocks[rs], cs)] - 1 if rs in h_blocks else W
+        cs = min(cs + int(l), nxt)
+    ans.append(f"{rs} {cs}")
+print(*ans, sep="\n")
