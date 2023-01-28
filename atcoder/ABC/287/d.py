50b8869ef08ed468fe3a5d2c209258bf68695616
kazu0716 <kazu2055.tennis@gmail.com>
2023-01-28 23:52:22 +0900

    fix: ABC287 d

--- atcoder/ABC/287/d.py
+++ atcoder/ABC/287/d.py
@@ -1,23 +1,15 @@
-from collections import deque
-
 S = input()
 T = input()
-back = deque(list(S[-len(T):]))
 cnt = 0
 for i, s in enumerate(S[-len(T):]):
     if T[i] == s or (s == "?" or T[i] == "?"):
         cnt += 1
 ans = ["Yes" if cnt == len(T) else "No"]
-for x in range(1, len(T) + 1):
-    idx = x - 1
-    added = S[idx]
-    poped = back.popleft()
-    if T[idx] == "?":
-        pass
-    else:
-        if poped == "?" or poped == T[idx]:
-            cnt -= 1
-        if added == "?" or added == T[idx]:
-            cnt += 1
+for idx in range(len(T)):
+    added, popped = S[idx], S[-len(T) + idx]
+    if T[idx] != "?" and (popped == "?" or popped == T[idx]):
+        cnt -= 1
+    if T[idx] != "?" and (added == "?" or added == T[idx]):
+        cnt += 1
     ans.append("Yes" if cnt == len(T) else "No")
 print(*ans, sep="\n")
