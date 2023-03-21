b9318be0a61dad7ba857866fd76999edd514ce03
kazu0716 <kazu2055.tennis@gmail.com>
2023-03-22 02:15:45 +0900

    fix: ABC294 d to adopt liner case

--- atcoder/ABC/294/d.py
+++ atcoder/ABC/294/d.py
@@ -1,21 +1,27 @@
 from collections import defaultdict
-from heapq import heapify, heappop, heappush
+from enum import IntEnum
+
+
+class PersonStatus(IntEnum):
+    NOT_CALLED = 0
+    CALLED = 1
+    GONE = 2
+
 
 N, Q = map(int, input().split())
-not_called, called = [i for i in range(1, N + 1)], []
-is_gone = defaultdict(bool)
-heapify(not_called)
+status = defaultdict(lambda: PersonStatus.NOT_CALLED)
+not_called_pos, called_pos = 1, 1
 ans = []
 for _ in range(Q):
     query = input().split()
     if query[0] == "1":
-        heappush(called, heappop(not_called))
+        while status[not_called_pos] != PersonStatus.NOT_CALLED:
+            not_called_pos += 1
+        status[not_called_pos] = PersonStatus.CALLED
     elif query[0] == "2":
-        is_gone[int(query[1])] = True
+        status[int(query[1])] = PersonStatus.GONE
     else:
-        while called:
-            if not is_gone[called[0]]:
-                ans.append(called[0])
-                break
-            heappop(called)
+        while status[called_pos] != PersonStatus.CALLED:
+            called_pos += 1
+        ans.append(called_pos)
 print(*ans, sep="\n")
