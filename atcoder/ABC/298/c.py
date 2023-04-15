9a2e2216446398580f1991a9181c367425155e76
kazu0716 <kazu2055.tennis@gmail.com>
2023-04-16 00:55:18 +0900

    add: ABC298 c

---
+++ atcoder/ABC/298/c.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+
+N = int(input())
+Q = int(input())
+box = defaultdict(list)
+card, card_set = defaultdict(list), defaultdict(set)
+for _ in range(Q):
+    query = list(map(int, input().split()))
+    i = query[1]
+    if query[0] == 1:
+        j = query[2]
+        box[j].append(i)
+        if j not in card_set[i]:
+            card[i].append(j)
+            card_set[i].add(j)
+    elif query[0] == 2:
+        box[i].sort()
+        print(*box[i])
+    else:
+        card[i].sort()
+        print(*card[i])
