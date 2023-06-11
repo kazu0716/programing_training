bf98a60075e43571468532a678a7ae7539fa2882
kazu0716 <kazu2055.tennis@gmail.com>
2023-06-11 14:33:54 +0900

    fix: ABC305 e

--- atcoder/ABC/305/e.py
+++ atcoder/ABC/305/e.py
@@ -1,7 +1,6 @@
-from heapq import heapify, heappop, heappush
+from collections import deque
 
 N, M, K = map(int, input().split())
-INF = pow(10, 18)
 edge = [[] for _ in range(N)]
 for _ in range(M):
     a, b = map(int, input().split())
@@ -11,20 +10,20 @@ guards = [-1] * N
 for _ in range(K):
     p, h = map(int, input().split())
     guards[p - 1] = h
-guard_hp = [INF] * N
-heap = [(-h, p) for p, h in enumerate(guards) if h > 0]
-heapify(heap)
-while heap:
-    hp, cur = heappop(heap)
-    if guard_hp[cur] <= hp:
+cost = [-1] * N
+for root, ini_hp in sorted([(p, h) for p, h in enumerate(guards) if h > 0], key=lambda x: x[1], reverse=True):
+    if cost[root] >= ini_hp:
         continue
-    guard_hp[cur] = hp
-    for nxt in edge[cur]:
-        nxt_hp = max(-hp - 1, guards[nxt])
-        if nxt_hp < 0 or guard_hp[nxt] <= nxt_hp:
-            continue
-        guard_hp[nxt] = nxt_hp
-        heappush(heap, (-nxt_hp, nxt))
-ans = [i + 1 for i, hp in enumerate(guard_hp) if hp < INF]
+    deq = deque([(root, ini_hp)])
+    cost[root] = ini_hp
+    while deq:
+        cur, hp = deq.popleft()
+        for nxt in edge[cur]:
+            nxt_hp = max(hp - 1, guards[nxt])
+            if nxt_hp < 0 or cost[nxt] >= nxt_hp:
+                continue
+            cost[nxt] = nxt_hp
+            deq.append((nxt, nxt_hp))
+ans = [i + 1 for i, c in enumerate(cost) if c > -1]
 print(len(ans))
 print(*ans)
