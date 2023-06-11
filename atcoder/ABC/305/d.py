e7ebf705058ecf76d978b493873172a22d52ea45
kazu0716 <kazu2055.tennis@gmail.com>
2023-06-11 13:59:54 +0900

    fix: ABC305 d

--- atcoder/ABC/305/d.py
+++ atcoder/ABC/305/d.py
@@ -2,20 +2,16 @@ from bisect import bisect_left
 
 
 def get_accum_idx(i):
-    if i % 2 == 0:
-        return i // 2 - 1
-    return (i - 1) // 2
+    return i // 2 - 1 if i % 2 == 0 else (i - 1) // 2
 
 
 N = int(input())
 A = list(map(int, input().split()))
 sleep_time = [0]
-sleep_list = []
 for i in range(N - 1):
     if i % 2 == 0:
         continue
     sleep_time.append(sleep_time[-1] + A[i + 1] - A[i])
-    sleep_list.append((A[i], A[i + 1]))
 Q = int(input())
 ans = []
 for _ in range(Q):
@@ -23,15 +19,12 @@ for _ in range(Q):
     l_idx, r_idx = bisect_left(A, l), bisect_left(A, r)
     if r_idx % 2 == 1:
         r_idx -= 1
-    if l_idx == 0:
-        l_idx += 1
     if l_idx % 2 == 0 and l == A[l_idx]:
         l_idx += 1
-    l_idx_accum, r_idx_accum = get_accum_idx(l_idx), get_accum_idx(r_idx)
-    sleep = sleep_time[r_idx_accum + 1] - sleep_time[l_idx_accum]
+    sleep = sleep_time[get_accum_idx(r_idx) + 1] - sleep_time[get_accum_idx(l_idx)]
     if l_idx % 2 == 0 and l < A[l_idx]:
-        sleep -= l - sleep_list[l_idx_accum][0]
+        sleep -= l - A[l_idx - 1]
     if r_idx % 2 == 0 and r < A[r_idx]:
-        sleep -= sleep_list[r_idx_accum][1] - r
+        sleep -= A[r_idx] - r
     ans.append(sleep)
 print(*ans, sep="\n")
