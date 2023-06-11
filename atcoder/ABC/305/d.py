da1c2c3a69cf02a48a2d3782fd13eb0b8fe9a49b
kazu0716 <kazu2055.tennis@gmail.com>
2023-06-11 14:14:05 +0900

    fix: add notes to ABC305 d

--- atcoder/ABC/305/d.py
+++ atcoder/ABC/305/d.py
@@ -1,7 +1,8 @@
 from bisect import bisect_left
 
 
-def get_accum_idx(i):
+def cast_sleep_time_idx(i):
+    """get sleep time index from idx of A"""
     return i // 2 - 1 if i % 2 == 0 else (i - 1) // 2
 
 
@@ -17,11 +18,15 @@ ans = []
 for _ in range(Q):
     l, r = map(int, input().split())
     l_idx, r_idx = bisect_left(A, l), bisect_left(A, r)
+    # NOTE: change idx of getting up at last time
     if r_idx % 2 == 1:
         r_idx -= 1
+    # NOTE: change idx of going bed at first time
     if l_idx % 2 == 0 and l == A[l_idx]:
         l_idx += 1
-    sleep = sleep_time[get_accum_idx(r_idx) + 1] - sleep_time[get_accum_idx(l_idx)]
+    # NOTE: get total sleep time by cumulative sum
+    sleep = sleep_time[cast_sleep_time_idx(r_idx) + 1] - sleep_time[cast_sleep_time_idx(l_idx)]
+    # NOTE: decrease surplus sleep time, when l or r is in sleeping time
     if l_idx % 2 == 0 and l < A[l_idx]:
         sleep -= l - A[l_idx - 1]
     if r_idx % 2 == 0 and r < A[r_idx]:
