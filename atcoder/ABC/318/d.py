dde11e8bd9b9f185afc5c827943303c780980e0e
kazu0716 <kazu2055.tennis@gmail.com>
2023-09-03 03:24:34 +0900

    add: ABC318 d

---
+++ atcoder/ABC/318/d.py
@@ -0,0 +1,39 @@
+from sys import setrecursionlimit
+
+from pypyjit import set_param
+
+set_param("max_unroll_recursion=-1")
+setrecursionlimit(pow(10, 9))
+
+
+def dfs(visited, cost):
+    global ans
+
+    ans = max(ans, cost)
+    if all(visited):
+        return
+    i = visited.index(False)
+    visited[i] = True
+    for j in range(i + 1, N):
+        if not visited[j]:
+            visited[j] = True
+            dfs(visited, cost + D[i][j])
+            visited[j] = False
+    visited[i] = False
+
+
+N = int(input())
+D = [[0] * N for _ in range(N)]
+for i in range(N - 1):
+    for j, d in enumerate(map(int, input().split())):
+        D[i][j + i + 1] = D[j + i + 1][i] = d
+ans = 0
+if N % 2 == 0:
+    visited = [False] * N
+    dfs(visited, 0)
+    exit(print(ans))
+for i in range(N):
+    visited = [False] * N
+    visited[i] = True
+    dfs(visited, 0)
+print(ans)
