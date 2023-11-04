0a4e583542d04d2cb70ad5e2279c747fa577575f
kazu0716 <kazu2055.tennis@gmail.com>
2023-11-05 03:03:43 +0900

    fix: ABC327 d

--- atcoder/ABC/327/d.py
+++ atcoder/ABC/327/d.py
@@ -4,7 +4,7 @@ N, M = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 X = [-1] * N
-graph = [set() for _ in range(N)]
+graph: list[set[int]] = [set() for _ in range(N)]
 for a, b in zip(A, B):
     graph[a - 1].add(b - 1)
     graph[b - 1].add(a - 1)
@@ -14,12 +14,11 @@ for i in range(N):
     X[i] = 0
     deq = deque([(i, X[i])])
     while deq:
-        cur, v = deq.popleft()
+        cur, bit = deq.popleft()
         for nxt in graph[cur]:
-            nxt_v = int(not bool(v))
             if X[nxt] == -1:
-                X[nxt] = nxt_v
-                deq.append((nxt, nxt_v))
-            elif X[nxt] != nxt_v:
+                X[nxt] = ~bit & 1
+                deq.append((nxt, X[nxt]))
+            elif X[nxt] != ~bit & 1:
                 exit(print("No"))
 print("Yes")
