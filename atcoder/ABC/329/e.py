d32879981a467036358da74402d7a163347d55de
kazu0716 <kazu2055.tennis@gmail.com>
2023-11-19 01:51:44 +0900

    fix: ABC329 e

--- atcoder/ABC/329/e.py
+++ atcoder/ABC/329/e.py
@@ -17,9 +17,11 @@ deq = deque([i for i in range(N - M + 1) if S[i:i + M] == T])
 while deq:
     head = deq.popleft()
     S[head:head + M] = ["#"] * M
+    # NOTE: check if the around range of center of initial pos can replace
     for di in range(-M + 1, M + 1):
         if 0 > head + di or head + di > N - M:
             continue
         if can_replace(S[head + di:head + di + M], T):
+            # NOTE: need to search them from head for AC
             deq.appendleft(head + di)
 print("Yes" if S == ["#"] * N else "No")
