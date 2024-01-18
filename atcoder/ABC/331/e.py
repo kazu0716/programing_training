8ee0adebecf778b8e62992b018dcc53ab801c262
kazu0716 <kazu2055.tennis@gmail.com>
2024-01-19 01:49:52 +0900

    fix: ABC331 e

--- atcoder/ABC/331/e.py
+++ atcoder/ABC/331/e.py
@@ -4,8 +4,8 @@ B = list(map(int, input().split()))
 CD = set([tuple(map(int, input().split())) for _ in range(L)])
 sorted_B = sorted([(b, i) for i, b in enumerate(B)], reverse=True)
 ans = 0
+# NOTE: Follow loop process run under O(N + L), because len(CD) is under L
 for i, a in enumerate(A):
-    # NOTE: Follow loop process run under O(L)
     for b, j in sorted_B:
         # NOTE: A,B are 0-indexed, but CD is 1-indexed
         if (i + 1, j + 1) not in CD:
